c1f1a183d5b88396b636ae3574f6135519a95eda
wharton-git <whartonaldrick@gmail.com>
2024-05-10 02:27:04 +0300

    Add : Footer wave

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import wave from './../assets/images/FooterWave.svg'
 
 const Footer = () => {
     return (
-        <div className='bg-gray-800 text-white'>
-            Footer
-        </div>
+        <>
+            <div className='m-0 bg-gradient-to-t from-gray-800 from-20% to-gray-100 to-80%'>
+                <img src={wave} alt="" className='' />
+                <div className='text-white'>
+                    Footer
+                </div>
+            </div>
+
+        </>
     )
 }
 
