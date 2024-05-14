8dd6ae7c74abcdbde8308975aaac89156284ab76
wharton-git <whartonaldrick@gmail.com>
2024-05-15 00:23:37 +0300

    Edit : Design Details

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import url from './../Api/http'
-import {  } from '@fortawesome/free-solid-svg-icons';
+import { ShoppingCart } from 'lucide-react';
 
 const Detail = () => {
 
@@ -26,26 +25,57 @@ const Detail = () => {
     }
 
     return (
-        <div className='w-3/4 mx-auto pt-10 sm:p-10'>
-            <div className='p-2 rounded-md bg-gray-200'>
-                <div className='flex space-x-4'>
-                    <div className='w-1/3'>
-                        <img src={"http://localhost:8000/images/" + detailProduits.image} alt={detailProduits.designation}
-                            className='rounded-md border-4 border-gray-800'
-                        />
-                    </div>
-                    <div>
-                        <div className='sm:text-2xl font-bold'>
-                            {detailProduits.designation}
-                        </div>
-                        <div>
-                            <FontAwesomeIcon icon={} size='xs' />
-                        </div>
-                    </div>
+        <div className='w-full sm:w-5/6 sm:mx-auto p-4 transition-all space-y-10'>
+            <div className='flex space-x-6'>
+                <div className='min-w-10 w-1/2 max-w-72'>
+                    <img src={"http://localhost:8000/images/" + detailProduits.image} alt={detailProduits.designation}
+                        className='mx-auto rounded-lg border-4 border-gray-800'
+                    />
                 </div>
                 <div>
-                    {detailProduits.description}
+                    <div className='font-bold my-1 sm:my-3 sm:text-xl md:text-2xl lg:text-3xl transition-all'>
+                        {detailProduits.designation}
+                    </div>
+                    <div className='sm:space-y-4'>
+                        <div className='space-x-4 text-base'>
+                            <span className='underline'>
+                                Prix :
+                            </span>
+                            <span className='font-bold'>
+                                {detailProduits.prix} $
+                            </span>
+                        </div>
+                        <div className='space-x-4 text-base'>
+                            <span className='underline'>
+                                Disponible :
+                            </span>
+                            <span className='font-bold'>
+                                {detailProduits.stock}
+                            </span>
+                        </div>
+                        <div className='space-x-4 text-base'>
+                            <span className='underline'>
+                                Details :
+                            </span>
+                            <span className='font-bold'>
+                                {detailProduits.description}
+                            </span>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div>
+                <button className='flex items-center bg-gray-800 text-white p-2 rounded-md shadow-lg shadow-gray-400 hover:shadow-gray-600 hover:scale-110 active:scale-100 active:bg-white active:text-black active:shadow-inner active:shadow-gray-500 transition-all'>
+                    <div>
+                        <ShoppingCart />
+                    </div>
+                    <div>
+                        Ajouter au panier
+                    </div>
+                </button>
+            </div>
+            <div>
+                Les prix des articles vendus sur R Market incluent la TVA. En fonction de votre adresse de livraison, la TVA peut varier au moment du paiement.
             </div>
         </div>
     )
